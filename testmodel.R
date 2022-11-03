cf568bf03c6b272adeb3bddeedc4427684be3cd8
robertladwig <ladwigjena@gmail.com>
2022-11-03 08:52:54 -0500

    prototype

--- testmodel.R
+++ testmodel.R
@@ -3,9 +3,11 @@ library(tidyverse)
 # dO2/dt = Flux * (O2 / (Khalf + O2)) * Theta^(Temp - 20) * Area
 # g/day = g/m2/day  * m2
 
+Output <- c(NULL)
 
+for (k in 1:100){
 # Normal distributions for parameter assumptions
-Flux <- rnorm(1, mean = -0.32, sd = -0.096)  # (g / m2 / d)
+Flux <- rnorm(1, mean = -0.32, sd = 0.096)  # (g / m2 / d)
 Khalf <- rnorm(1, mean = 0.224, sd = 0.032)   # (g / m3)
 Theta <- rnorm(1, mean = 1.07, sd = 0.03) # (-)
 
@@ -24,8 +26,9 @@ Depth <- as.numeric(Depth_raw[1, -1]) * (-1)
 Area_raw <- read_delim('InputTest/temperate/hypsograph.dat', skip = 1, delim = ' ',
                        col_names = F)
 
-Area = data.frame('Depth' = (Area_raw[, 1] - max(Area_raw[, 1])) * (-1), 'Area' = Area_raw[, 2])
-Area = apply(Area, 2, rev)
+Area_list = data.frame('Depth' = (Area_raw[, 1] - max(Area_raw[, 1])) * (-1), 'Area' = Area_raw[, 2])
+Area_list = apply(Area_list, 2, rev)
+Area_list <- as.data.frame(Area_list)
 
 Temp <- Temp_raw
 colnames(Temp) <- c('Datetime', paste0('wtr_', Depth))
@@ -54,24 +57,53 @@ ggplot(subset(Thermocline.depth, datetime > as.Date('2000-01-01') &
   geom_smooth() + 
   scale_y_reverse()
 
+Thermocline.PK <- read.csv('InputTest/temperate/thermo_information.csv')
 
-Volume <- # VOLUME FROM AREA DEPENDING ON THERMOCLINE DEPTH
-Area <- # ACTIVE AREA FOR SEDIMENT FLUX
+Strat.Period <- (na.contiguous(Thermocline.PK$stratified))
 
-Oxygen <- rep(NA, length(SOMETHING))
+Thermocline.PK <- Thermocline.PK[attributes(Strat.Period)$tsp[1]: 
+                         attributes(Strat.Period)$tsp[2],]
+
+Area_interp <- approx(Area_list$X1, Area_list$X2, seq(0, max(Area_list$X1), 0.1))$y
+Area_interp <- data.frame('Depth' = seq(0, max(Area_list$X1), 0.1), 'Area' =
+                          Area_interp)
+Area_interp$Volume <- rev(cumsum(Area_interp$Depth * Area_interp$Area))
+
+Volume <- approx(Area_interp$Depth, Area_interp$Volume, Thermocline.PK$thermocline_depth_smooth,
+                 rule =2)$y # VOLUME FROM AREA DEPENDING ON THERMOCLINE DEPTH
+Area <- approx(Area_interp$Depth, Area_interp$Area, Thermocline.PK$thermocline_depth_smooth,
+               rule = 2)$y # ACTIVE AREA FOR SEDIMENT FLUX
+
+Temp <- Thermocline.PK$hypo_temp
+
+Oxygen <- rep(NA, length(Volume))
 
 Oxygen[1] <- o2.at.sat.base(temp = Temp[1], altitude = 500) * Volume[1] # g
   
-for (i in 2:SOMETHING){
+for (i in 2:length(Oxygen)){
   
   Oxygen[i] <- (Oxygen[i - 1] + Area[i - 1] * (Flux * 
-                  (output[i - 1] / (Khalf + output[i - 1] )) * 
-                  Theta^(Temp - 20)) * dt ) * Volume[n] / Volume[n-1]
+                  (Oxygen[i - 1] / (Khalf + Oxygen[i - 1] )) * 
+                  Theta^(Temp[i-1] - 20)) * dt ) * Volume[i] / Volume[i-1]
   
 }
 
 
 
+Output <- cbind(Output, Oxygen / Volume)
+
+}
+
+Output_df = data.frame('Time' = Thermocline.PK$datetime, Output)
+
+m.Output_df = pivot_longer(Output_df, 2:last_col())
+m.Output_df <- m.Output_df %>% 
+  mutate(Time = lubridate::as_datetime(Time))
+
+ggplot(m.Output_df) +
+  geom_line(aes(Time, value, group = name)) +
+  theme_bw() + xlab('Date') + ylab('O2 [mg/L]') #+
+  # scale_x_datetime(date_labels = "%M-")
 
 
 
