e3be299984a81bb4275eb335d89ba2fa92204ee3
Philipp Keller <philipp.keller@usherbrooke.ca>
2022-11-18 11:29:04 -0500

    merge

--- lake_processing.R
+++ lake_processing.R
@@ -4,18 +4,22 @@ library(LakeMetabolizer)
 library(lubridate)
 library(here)
 library(purrr)
+library(lubridate)
+library(pracma)
+library(tictoc)
 
 source(here("R/thermal_script.R"))
+source(here("R/thermocline_helper.R"))
 
 lake_folder <- "InputTest"
 lakes <- list.files(here(lake_folder), full.names = T)
-
 
 lakes %>% 
   walk(~{
     lake <- .x
     
     # writes thermal_info.csv into lake folder
+    tic(msg = basename(lake))
     lake %>% thermal_info()
-    
-  })
+    toc()
+})
