e340c5f85c005192b47e641679c2e8e39858f7df
Philipp Keller <philipp.keller@usherbrooke.ca>
2022-12-10 17:12:19 -0500

    error handling

--- _targets.R
+++ _targets.R
@@ -110,7 +110,7 @@ targets <- tar_map(
   tar_target(oxy_quality, oxy_qa(oxygen, observations)),
   tar_target(oxy_scatter, oxy_qa_full(oxygen, observations)),
   # Create QC plots for oxygen
-  tar_target(plot_qc_oxy, save_qc_plot_oxygen(oxygen, lake_id, observations), format = "file"),
+  tar_target(plot_qc_oxy, save_qc_plot_oxygen(oxygen, lake_id, observations), format = "file", error = "null"),
   # Create plots of temperature and thermocline depth
   tar_target(plot_thermal, create_plots_thermal(thermal, lake_id, lake_folder), format = "file")
 )
