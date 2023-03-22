656afbd86f934f4c04a59e4cc1012da614b267ab
Philipp Keller <philipp.keller@usherbrooke.ca>
2023-03-22 12:03:52 -0400

    patch

--- _targets.R
+++ _targets.R
@@ -52,8 +52,8 @@ tar_config_set(store = "~/scratch/isioxy/") # Folder for target's internal data 
 # )
 
 # tar_make_clustermq() configuration:
-options(clustermq.scheduler = "multicore") # parallel processing on local machine
-# options(clustermq.scheduler = "slurm") # Slurm on HPC
+# options(clustermq.scheduler = "multicore") # parallel processing on local machine
+options(clustermq.scheduler = "slurm") # Slurm on HPC
 options(clustermq.template = "clustermq.tmpl") # Slurm sbatch template
 
 # source required functions from R subfolder
