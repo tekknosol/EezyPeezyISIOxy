2f40ad2d77cfe8369afee57660341318347d7c15
Philipp Keller <philipp.keller@igb-berlin.de>
2024-05-14 22:42:16 +0200

    test run

--- hpc/full_run.sh
+++ hpc/full_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=5
-#SBATCH --mem=0
+#SBATCH --mem=16G
 #SBATCH --time=00:30:00
 #SBATCH --mail-user=philipp.keller@igb-berlin.de
 #SBATCH --mail-type=ALL
